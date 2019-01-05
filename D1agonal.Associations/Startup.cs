2ef6627b0246e24aad92f6513d4aa30977151e07
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2019-01-04 23:30:14 -0500

    Add associations.json

--- D1agonal.Associations/Startup.cs
+++ D1agonal.Associations/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -40,7 +41,6 @@ namespace D1agonal.Associations
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseMvc();
         }
